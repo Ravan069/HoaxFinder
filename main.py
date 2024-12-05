8cd45a028d9529d16e47130d70175fcac99116cb
Ravan069 <133179897+Ravan069@users.noreply.github.com>
2024-12-05 07:47:48 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from GoogleNews import GoogleNews
+import streamlit as st
+from sentence_transformers import CrossEncoder
+
+st.set_page_config(page_title="HoaxFinder", page_icon="🕵️", layout="wide")
+st.header("🕵️ HoaxFinder ")
+
+model = CrossEncoder('abbasgolestani/ag-nli-DeTS-sentence-similarity-v4')
+googlenews = GoogleNews()
+
+def fetch_news(query):
+    googlenews.search(query)
+    return googlenews.get_texts()
+
+def valid(query, news):   
+    pairs = zip(query, news)
+    list_pairs=list(pairs)
+    scores1 = model.predict(list_pairs, show_progress_bar=False)
+    
+    sum=0 
+    for score in scores1:
+        sum+=score
+        
+    print_news = str(news)    
+         
+    result = sum/len(scores1)
+         
+    if result >= 0.8 :
+        st.write("""
+                ### 🕵️ says the news is Hoax!!! 
+                 
+                ## 📰 According to Google News:
+                 
+                 """, print_news[2:-2],  sum)
+    else :
+        st.write("""
+                ### 🕵️ says the news is Not a Hoax!!! 
+                 
+                ## 📰 According to Google News:
+                 
+                 """, print_news[2:-2],  sum)   
+              
+
+if __name__ == "__main__":
+    
+    if query := st.text_input("Enter the news title"):
+        google_news = fetch_news(query)
+        valid(query, google_news)
